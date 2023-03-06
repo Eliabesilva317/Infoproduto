264357c329c088a628356cf3e6feec67fcb92ebb
unknown <Savioanjos2020@gmail.com>
2023-03-06 16:02:04 -0300

    Configurando package.json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import { PrismaClient  } from '@prisma/client';
 import fastify from 'fastify';
+import z from 'zod';
 
 const app = fastify();
 
@@ -9,5 +10,30 @@ app.get('/products', async () => {
     const users = await prisma.user.findMany();
 
     return { users }
+});
+
+app.post('/products', async (request, reply) => {
+    const createProductSchema = z.object({
+        name: z.string(),
+        price: z.number(),
+    })
+
+    const { name, price } = createProductSchema.parse(request.body)
+
+    await prisma.user.create({
+        data: {
+            name,
+            price,
+        }
+    })
+
+    return reply.status(201).send()
+})
+
+app.listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(() => {
+    console.log('HTTP Server Running')
 })
 
