2b6f71a09e8fec1e09218ddb295d8c0df5157962
unknown <Savioanjos2020@gmail.com>
2023-03-06 21:27:56 -0300

    Adicionando imagem da capa

--- web/card-web/src/App.js
+++ web/card-web/src/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
+import img from "./assets/preview.gif";
 
 function App() {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -24,30 +23,31 @@ function App() {
   return (
     <div className="container">
       <div className="left-pane">
-      <h1 className='top'>Login</h1>
-    <div>  
-      <input className='input'
-          type="email"
-          id="email-input"
-          value={email}
-          onChange={handleEmailChange}
-          autoComplete="username"
-        /> 
+        <h1 className="top">Login</h1>
+        <div>
+          <input
+            className="input"
+            type="email"
+            id="email-input"
+            value={email}
+            onChange={handleEmailChange}
+            autoComplete="username"
+          />
         </div>
-        <div className='login'>
-          <input className='input'
-          type="password"
-          id="password-input"
-          value={password}
-          onChange={handlePasswordChange}
-          autoComplete="current-password"
-        />
+        <div className="login">
+          <input
+            className="input"
+            type="password"
+            id="password-input"
+            value={password}
+            onChange={handlePasswordChange}
+            autoComplete="current-password"
+          />
         </div>
       </div>
 
-
-      <div className="right-pane"> 
-   
+      <div className="right-pane">
+        <img src={img} alt="Capa" />
       </div>
     </div>
   );
