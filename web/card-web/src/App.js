ad312643951cec0986bd6365d71932400ec92afc
Eliabesilva317 <eliabecruzsilva40@gmail.com>
2023-03-07 22:30:42 -0300

    fix commit navegaçoes router

--- web/card-web/src/App.js
+++ web/card-web/src/App.js
@@ -1,61 +1,21 @@
-import React, { useState } from "react";
-import "./App.css";
-import img from "./assets/preview.gif";
+import {BrowserRouter, Routes, Route,} from "react-router-dom";
+import Login from "./pages/login";
 
-function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+export function App() {
+  return(
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+    <BrowserRouter>
+      <div className="container">
+       <Routes>
+         <Route path='/' element={<Login />}/>
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
-    // Adicione aqui o código para enviar o email e senha para o servidor
-  };
-
-  return (
-    <div className="container">
-      <div className="left-pane">
-        <h1 className="top">Login</h1>
-        <div>
-          <input
-            className="input"
-            type="email"
-            id="email-input"
-            value={email}
-            onChange={handleEmailChange}
-            autoComplete="username"
-          />
-        </div>
-        <div className="login">
-          <input
-            className="input"
-            type="password"
-            id="password-input"
-            value={password}
-            onChange={handlePasswordChange}
-            autoComplete="current-password"
-          />
-        </div>
-
-        <div>
-      <button className="button">Entra</button>
+         {/* <Route path='/about' element={<About />}/> */}
+       </Routes>
       </div>
+  </BrowserRouter>
 
-      </div>
-
-      <div className="right-pane">
-        <img className="image" src={img} alt="Capa" />
-      </div>
-    </div>
-  );
+  )
 }
 
 export default App;
