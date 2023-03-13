92c11292a400ba6d7c17e46c2bd33b926f3b3f41
Eliabesilva317 <eliabecruzsilva40@gmail.com>
2023-03-13 15:20:25 -0300

    fix commit

--- web/card-web/src/pages/Home/index.js
+++ web/card-web/src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "../Home/index.css";
-import img from "../../assets/foguete.gif";
+import img from "../../assets/preview-hamburger.gif";
+import { Element } from 'react-scroll';
+
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -55,24 +57,34 @@ function Home() {
 
       {/* Space down */}
 
+      <Element id="meu-scroll" className="scroll-container">Conteúdo com scroll
       <div className="space-1">
+
         <div className="space-block">
-          
+        <div className="favorito" >
+             <img className="image" src={img} alt="Capa" />
+            </div>
+             <div className="deteles" />
            </div>  
 
           <div className="space-block">
-
+          <img className="image" src={img} alt="Capa" />
+              <div className="favorito" />
              </div>
 
         <div className="space-block">
-
-           </div>
-
+        <img className="image" src={img} alt="Capa" />
+             <div className="favorito" />
+          </div>
+           
         <div className="space-block">
-
+        <img className="image" src={img} alt="Capa" />
+        <div className="favorito" />
+             
            </div>
         
         </div> 
+        </Element>
     </div>
   );
 }
