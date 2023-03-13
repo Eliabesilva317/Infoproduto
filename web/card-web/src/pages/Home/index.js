037202a0dd3d0e583b4fc7a5db8e384f64cdf250
unknown <Savioanjos2020@gmail.com>
2023-03-13 10:19:02 -0300

    Estruturando modal

--- web/card-web/src/pages/Home/index.js
+++ web/card-web/src/pages/Home/index.js
@@ -76,22 +76,21 @@ function Home() {
           overlayClassName={"modal-overlay"}
           className="modal-content"
         >
-          <h2>Hello - I am a modal!</h2>
+          <h2>Faça seu pedido</h2>
           <hr />
+          <p>Nome: Batata Frita</p>
+
+          <p>Preço: 20</p>
+
           <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+            Quantidade: <input type="number" />
           </p>
+
           <button onClick={closeModal}>Close</button>
         </Modal>
-        <div className="space-block"></div>
-
-        <button onClick={openModal}>Open Modal</button>
+        <div className="space-block">
+          <button onClick={openModal}>Fazer pedido</button>
+        </div>
       </div>
     </div>
   );
