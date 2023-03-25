2d2b48723efe0abb5dbeb2f67045a03bf3d49ac0
unknown <Savioanjos2020@gmail.com>
2023-03-25 19:39:04 -0300

    Criando controle de visibilidade das categorias e produtos

--- web/card-web/src/pages/HomeAdm/index.js
+++ web/card-web/src/pages/HomeAdm/index.js
@@ -4,10 +4,21 @@ import styles from "./styles.module.scss";
 
 const HomeAdm = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [visibleCategory, setVisibleCategory] = useState(false);
+  const [visibleProduct, setVisibleProduct] = useState(true);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
+
+  const visibleCategoryController = () => {
+    setVisibleCategory(!visibleCategory);
+  };
+
+  const visibleProductController = () => {
+    setVisibleProduct(!visibleProduct);
+  };
+
   return (
     <div className={styles.container}>
       <div className="row"></div>
@@ -39,7 +50,11 @@ const HomeAdm = () => {
         <h1>Bem vindo!</h1>
         <h2>Quais itens você deseja modificar?</h2>
 
-        <button type="button" className={styles.buttonCategory}>
+        <button
+          type="button"
+          className={styles.buttonCategory}
+          onClick={visibleCategoryController}
+        >
           Categorias
         </button>
 
@@ -47,45 +62,49 @@ const HomeAdm = () => {
           Produtos
         </button>
 
-        <div className={styles.contentCategories}>
-          <div className={`${styles.category} ${styles.registerCategory}`}>
-            <h1>Cadastrar categoria:</h1>
-            <form>
-              <input type="text" placeholder="Digite o nome da categoria" />
-              <br />
+        {/* Area de categorias */}
+
+        {visibleCategory && (
+          <div className={styles.contentCategories}>
+            <div className={`${styles.category} ${styles.registerCategory}`}>
+              <h1>Cadastrar categoria:</h1>
+              <form>
+                <input type="text" placeholder="Digite o nome da categoria" />
+                <br />
+                <button type="submit" className={styles.buttonProduct}>
+                  Cadastrar
+                </button>
+              </form>
+            </div>
+
+            <div className={`${styles.category} ${styles.listCategory}`}>
+              <h1>Visualizar categorias:</h1>
+
+              <h4>Clique no botão para ver todas as categorias.</h4>
+
               <button type="submit" className={styles.buttonProduct}>
-                Cadastrar
+                Visualizar
               </button>
-            </form>
+            </div>
+
+            <div className={`${styles.category} ${styles.deleteCategory}`}>
+              <h1>Deletar categoria:</h1>
+              <form>
+                <select>
+                  <option value="">Selecione uma categoria</option>
+                  <option value="1">Pizzas</option>
+                  <option value="2">Bebidas</option>
+                  <option value="3">Comidas</option>
+                  <option value="4">Outros</option>
+                </select>
+                <br />
+                <button type="submit" className={styles.buttonProduct}>
+                  Deletar
+                </button>
+              </form>
+            </div>
           </div>
-
-          <div className={`${styles.category} ${styles.listCategory}`}>
-            <h1>Visualizar categorias:</h1>
-
-            <h4>Clique no botão para ver todas as categorias.</h4>
-
-            <button type="submit" className={styles.buttonProduct}>
-              Visualizar
-            </button>
-          </div>
-
-          <div className={`${styles.category} ${styles.deleteCategory}`}>
-            <h1>Deletar categoria:</h1>
-            <form>
-              <select>
-                <option value="">Selecione uma categoria</option>
-                <option value="1">Pizzas</option>
-                <option value="2">Bebidas</option>
-                <option value="3">Comidas</option>
-                <option value="4">Outros</option>
-              </select>
-              <br />
-              <button type="submit" className={styles.buttonProduct}>
-                Deletar
-              </button>
-            </form>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
